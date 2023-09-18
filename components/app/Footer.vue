976ca9cd13513b77c588b56f8fcff4a5f4714b70
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-09-18 15:10:26 +0600

    footer completed

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -1,37 +1,49 @@
 <template>
-    <div class="mt-20 pt-5 pb-20 px-5 bg-brand text-white">
-        <div class="custom-container pt-10">
-           
-            <div class="mt-8 border border-t-0 border-x-0 border-b-gray-500 pb-4">
-                <div class="grid grid-cols-5">
-                    <div>
-                        <img src="/images/brand-logo.png" alt="Webiizard Logo" class="h-8">
+    <div class="mt-10 md:mt-20 py-10 md:py-20 px-5 bg-brand text-gray-300 text-sm">
+        <div class="custom-container">
+            <div class="border border-t-0 border-x-0 border-b-gray-500 pb-4">
+                <div class="grid grid-cols-1 md:grid-cols-5 space-y-3 md:space-y-0">
+                    <div class="flex justify-center md:justify-start">
+                        <img src="/images/brand-logo.png" alt="Webiizard Logo" class="h-6 lg:h-8">
                     </div>
                     <div class="flex justify-center">
-                        <div>
-                            <NuxtLink to="#" class="text-sm block mb-1 text-gray-300">Services</NuxtLink>
+                        <div class="text-center md:text-start">
+                            <NuxtLink to="#" class="text-sm block mb-2 md:mb-1 text-gray-300">Services</NuxtLink>
                             <NuxtLink to="#" class="text-sm block text-gray-300">Solutions</NuxtLink>
                         </div>
                     </div>
-                    <div class="flex justify-center border border-gray-500 border-y-0">
-                        <div class="">
-                            <NuxtLink to="#" class="text-sm block mb-1 text-gray-300">Projects</NuxtLink>
+                    <div class="flex justify-center border-0 md:border border-gray-500 md:border-y-0">
+                        <div class="text-center md:text-start">
+                            <NuxtLink to="#" class="text-sm block mb-2 md:mb-1 text-gray-300">Projects</NuxtLink>
                             <NuxtLink to="#" class="text-sm block text-gray-300">Blogs</NuxtLink>
                         </div>
                     </div>
-                    <div class="flex justify-center border border-gray-500 border-y-0 border-l-0">
-                        <div>
-                            <NuxtLink to="#" class="text-sm block mb-1 text-gray-300">About us</NuxtLink>
+                    <div class="flex justify-center border-0 md:border border-gray-500 md:border-y-0 md:border-l-0">
+                        <div class="text-center md:text-start">
+                            <NuxtLink to="#" class="text-sm block mb-2 md:mb-1 text-gray-300">About us</NuxtLink>
                             <NuxtLink to="#" class="text-sm block text-gray-300">Contact us</NuxtLink>
                         </div>
                     </div>
                     <div class="flex justify-center">
-                        <div>
-                            <NuxtLink to="#" class="text-sm block mb-1 text-gray-300">Privacy Policy</NuxtLink>
+                        <div class="text-center md:text-start">
+                            <NuxtLink to="#" class="text-sm block mb-2 md:mb-1 text-gray-300">Privacy Policy</NuxtLink>
                             <NuxtLink to="#" class="text-sm block text-gray-300">Terms & Conditions</NuxtLink>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="mt-4">
+                <div class="block sm:flex gap-2 justify-center">
+                    <p class="text-center">&copy;{{ new Date().getFullYear() }} Webiizard. All rights reserved.</p>
+                    <p class="text-center">Call us: <a href="tel:01700557663">01700557663</a></p>
+                </div>
+                <div class="flex items-center justify-center gap-4 mt-1">
+                    <!-- <span>Follow On:</span> -->
+                    <a href="#" target="_blank" class="font-semibold">Linkedin</a>
+                    <a href="#" target="_blank" class="font-semibold">Facebook</a>
+                    <a href="#" target="_blank" class="font-semibold">Instagram</a>
+                </div>
+                
             </div>
         </div>
     </div>
